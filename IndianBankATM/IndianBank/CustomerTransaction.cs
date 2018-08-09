2177aa81d3cb6c469c66ce1f4cafe30982d283d3
vinothkanth <vinothkanth@5gindia.net>
2018-08-09 19:14:32 +0530

    ATM Basic Fuction
    This is your first assessment question. Please finish and submit it by Friday morning (10.08.2018).
    
    Develop a software which has similar functionality of ATM machine.
    
    like,
    
    1. Withdrawal,
    
    2. Mini statement,
    
    3. Change pin
    
    4. Deposit,
    
    5. Check balance.
    
    Assume that There is only one user and below are the user information.
    
    ATM-PIN : <any four digit number>
    
    Name : <your name>
    
    Default balance : 100000
    
    User should be able to perform the operations mentioned above. Please apply the concepts you learned so far and it should have unit test suites as well.

---
+++ IndianBankATM/IndianBank/CustomerTransaction.cs
@@ -0,0 +1,139 @@
+﻿// ===============================
+// Title of the Assesment : ATM Functionality
+// Auther                 : Vinoth Kanth
+// Starting Date          : 7/8/2018
+//
+// This class file is used to interact and read data from the user,
+// it also used to transfer the data to ATM class and retrive the corresponding data 
+//
+// ================================
+
+/// <summary>
+/// The IndianBank NameSpace
+/// </summary>
+namespace IndianBank
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// The Customer Transaction class
+    /// </summary>
+    class CustomerTransaction
+    {
+        /// <summary>
+        /// The User Choice enum 
+        /// </summary>
+        enum AtmMenu
+        {
+            Withdrawal = 1,
+            MiniStatement,
+            ChangePin,
+            Deposit,
+            CheckBalance
+        };
+
+        /// <summary>
+        /// The Main Fuction
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            AutomatedTellerMachine atm = new AutomatedTellerMachine(  );
+
+            // To set the customer details
+            Console.WriteLine( "Enter your name" );
+            string customerName = Convert.ToString( Console.ReadLine( ) );
+            Console.WriteLine( "Enter your ATM-PIN number :<any four digit number>" );
+            int setPinNumber = Convert.ToInt16( Console.ReadLine( ) );
+
+            atm.IsAddCustomerDetail( customerName, setPinNumber );
+
+            //Console.WriteLine("\n\n---------------------------------------------");
+            //Console.WriteLine("Welcome");
+            //Console.WriteLine("Your Account Name    : {0}", customerName);
+            //Console.WriteLine("Your ATM Pin Number  :{0}", setPinNumber);
+            //Console.WriteLine("Your Account Balance :{0}", 100000.00);
+            //Console.WriteLine("---------------------------------------------\n\n");
+
+            // To do atm functionality
+            Console.WriteLine( "Hi {0}", customerName );
+            Console.Write( "PLEASE ENTER YOUR 4 DIGIT PIN NUMBER : " );
+            int atmPinNumber = Convert.ToInt16( Console.ReadLine() );
+
+            bool isValidUser = atm.IsValidCustomer( customerName, atmPinNumber );
+
+            if ( isValidUser )
+            {
+                bool isProcessContinue = true;
+                while ( isProcessContinue )
+                {
+                    Console.WriteLine("1. Withdrawal");
+                    Console.WriteLine("2. Mini statement");
+                    Console.WriteLine("3. Change pin ");
+                    Console.WriteLine("4. Deposit");
+                    Console.WriteLine("5. Check balance. ");
+
+                    int userChoice = Convert.ToInt16( Console.ReadLine( ) );
+
+                    switch ( userChoice )
+                    {
+                        case (int)AtmMenu.Withdrawal:
+                            Console.Write( "ENTER WITHDRAW AMOUNT Rs." );
+                            double withdrawAmount = Convert.ToDouble( Console.ReadLine( ) );
+                            Console.WriteLine( atm.GetWithdrawAmount( customerName, atmPinNumber, withdrawAmount ) );
+                            break;
+
+                        case (int)AtmMenu.MiniStatement:
+                            Console.WriteLine( "Hi {0}", customerName );
+                            Console.WriteLine( atm.GetMiniStatement( customerName, atmPinNumber ) );
+                            Console.WriteLine( "YOUR ACCOUNT BALANCE {0}", atm.GetAccountBalance( customerName, atmPinNumber ) );
+                            break;
+
+                        case (int)AtmMenu.ChangePin:
+                            Console.WriteLine( "ENTER OLD PIN NUMBER" );
+                            int oldPin = Convert.ToInt16( Console.ReadLine( ) );
+                            Console.WriteLine( "ENTER NEW PIN NUMBER" );
+                            int newPin = Convert.ToInt16( Console.ReadLine( ) );
+                            Console.WriteLine( atm.ResetPinNumber( customerName, oldPin, newPin ) );
+                            atmPinNumber = newPin;
+                            break;
+
+                        case (int)AtmMenu.Deposit:
+                            Console.Write( "ENTER DEPOSIT AMOUNT Rs." );
+                            double depositAmount = Convert.ToDouble( Console.ReadLine( ) );
+                            Console.WriteLine( atm.AddDepositAmount( customerName, atmPinNumber, depositAmount ) );
+                            break;
+
+                        case (int)AtmMenu.CheckBalance:
+                            Console.WriteLine( "\n YOUR CURRENT BALANCE IS {0}", atm.GetAccountBalance( customerName, atmPinNumber ) );
+                            break;
+
+                        default:
+                            Console.WriteLine( "Hi {0}", customerName );
+                            Console.WriteLine( "ENTER VALID INPUT" );
+                            break;
+                    }
+
+                    Console.WriteLine( "DO YOU WANT TO CONTINUE  : yes/no" );
+                    string doAgain = Convert.ToString( Console.ReadLine( ) );
+                    if ( doAgain.ToLower( ).Contains( "yes" ) )
+                    {
+                        isProcessContinue = true;
+                    }
+                    else
+                    {
+                        isProcessContinue = false;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("PLEASE CHECK PIN NUMBER");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ IndianBankATM/IndianBankTestCase/AutomatedTellerMachineTestCase.cs
@@ -0,0 +1,209 @@
+﻿// ===============================
+// Title of the Assesment : ATM Functionality
+// Auther                 : Vinoth Kanth
+// Starting Date          : 7/8/2018
+//
+// It is an Test case file for Automated Teller Machine Class,
+// All members functions of ATM class are tested using mock value.
+// ================================
+
+/// <summary>
+/// The IndianBankTestCase Namespance
+/// </summary>
+namespace IndianBankTestCase
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using IndianBank;
+
+    /// <summary>
+    /// The Automated Teller Machine Test Case class
+    /// </summary>
+    [TestClass]
+    public class AutomatedTellerMachineTestCase
+    {
+        AutomatedTellerMachine atm = new AutomatedTellerMachine();
+
+        /// <summary>
+        /// To check the user details has been successfully added or not
+        /// </summary>
+        [TestMethod]
+        public void TestCheckAddCustomer()
+        {
+            string firstUser = "NVK";
+            string secondUser = "VVK";
+            int firstUserPinNumber = 1234;
+            int secondUserPinNumber = 5252;
+
+            Assert.IsTrue(atm.IsAddCustomerDetail(firstUser, firstUserPinNumber));
+            Assert.IsTrue(atm.IsAddCustomerDetail(secondUser, secondUserPinNumber));
+        }
+
+        /// <summary>
+        /// To check the given user details is valid or not
+        /// </summary>
+        [TestMethod]
+        public void TestCheckValidUser()
+        {
+            string userName = "NVK";
+            int userPinNumber = 1234;
+            Assert.IsTrue(atm.IsAddCustomerDetail(userName, userPinNumber));
+
+            Assert.IsTrue(atm.IsValidCustomer(userName, userPinNumber));
+            Assert.IsFalse(atm.IsValidCustomer(userName, 5252));
+            Assert.IsFalse(atm.IsValidCustomer("vvk", userPinNumber));
+        }
+
+        /// <summary>
+        /// To check the customer account balance
+        /// </summary>
+        [TestMethod]
+        public void TestCheckBalance()
+        {
+            string userName = "Vj";
+            int userPinNumber = 5252;
+            Assert.IsTrue(atm.IsAddCustomerDetail(userName, userPinNumber));
+
+            Assert.AreEqual("100000", atm.GetAccountBalance(userName, userPinNumber));
+            atm.GetWithdrawAmount(userName, userPinNumber, 4000);
+            Assert.AreEqual("96000", atm.GetAccountBalance(userName, userPinNumber));
+            Assert.AreNotEqual("2000", atm.GetAccountBalance(userName, userPinNumber));
+            Assert.AreEqual("PLEASE CHECK YOUR PIN-NUMBER", atm.GetAccountBalance("Name", userPinNumber));
+            Assert.AreEqual("PLEASE CHECK YOUR PIN-NUMBER", atm.GetAccountBalance(userName, 5766));
+        }
+
+        /// <summary>
+        /// To check the reset pin function 
+        /// </summary>
+        [TestMethod]
+        public void TestPinReset()
+        {
+            string userName = "Vj";
+            int userPinNumber = 5252;
+            Assert.IsTrue(atm.IsAddCustomerDetail(userName, userPinNumber));
+            string pinSummaryOne = atm.ResetPinNumber(userName, userPinNumber, 2525);
+            string pinSummaryTwo = atm.ResetPinNumber(userName, 3333, 2525);
+
+            Assert.AreEqual(" YOUR ATM-PIN NUMBER HAS BEEN RESETED SUCCESSFULLY..!!!", pinSummaryOne);
+            Assert.AreNotEqual("PLEASE CHECK YOUR PIN-NUMBER", pinSummaryOne);
+
+            Assert.AreNotEqual(" YOUR ATM-PIN NUMBER HAS BEEN RESETED SUCCESSFULLY..!!!", pinSummaryTwo);
+            Assert.AreEqual("PLEASE CHECK YOUR PIN-NUMBER", pinSummaryTwo);
+
+        }
+
+        /// <summary>
+        /// To check deposit method
+        /// </summary>
+        [TestMethod]
+        public void TestDeposit()
+        {
+            string userName = "Vk";
+            int userPinNumber = 5252;
+            Assert.IsTrue(atm.IsAddCustomerDetail(userName, userPinNumber));
+            string summaryOne = atm.AddDepositAmount(userName, userPinNumber, 5000.00);
+            string summaryTwo = atm.AddDepositAmount(userName, userPinNumber, -5000.00);
+            string summaryThree = atm.AddDepositAmount("Rk", 3388, 5000.00);
+
+            DateTime currentDateAndTime = DateTime.Now;
+            string statment = currentDateAndTime.ToString()+" "+5000+" + (DEPOSIT) ";
+
+            Assert.AreEqual(statment, summaryOne);
+            Assert.AreNotEqual("ENTER VALID AMOUNT", summaryOne);
+            Assert.AreNotEqual("PLEASE CHECK YOUR PIN-NUMBER", summaryOne);
+
+            Assert.AreEqual("ENTER VALID AMOUNT", summaryTwo);
+            Assert.AreNotEqual("PLEASE CHECK YOUR PIN-NUMBER", summaryTwo);
+            Assert.AreNotEqual(statment, summaryTwo); 
+
+            Assert.AreEqual("PLEASE CHECK YOUR PIN-NUMBER", summaryThree);
+            Assert.AreNotEqual("ENTER VALID AMOUNT", summaryThree);
+            Assert.AreNotEqual(statment, summaryThree); 
+
+        }
+
+        /// <summary>
+        /// To check Withdraw method
+        /// </summary>
+        [TestMethod]
+        public void TestWithdraw()
+        {
+            string userName = "Vk";
+            int userPinNumber = 5252;
+            Assert.IsTrue(atm.IsAddCustomerDetail(userName, userPinNumber));
+            string summaryOne = atm.GetWithdrawAmount(userName, userPinNumber, 5000.00);
+            string summaryTwo = atm.GetWithdrawAmount(userName, userPinNumber, -5000.00);
+            string summaryThree = atm.GetWithdrawAmount("Rk", 3388, 5000.00);
+            string summaryFour = atm.GetWithdrawAmount(userName, userPinNumber, 10000000);
+            string summaryFive = atm.GetWithdrawAmount(userName, userPinNumber, 640);
+
+            DateTime currentDateAndTime = DateTime.Now;
+            string statment = currentDateAndTime.ToString() + " " + 5000 + " -( WITHDRAW ) ";
+            
+            Assert.AreEqual(statment, summaryOne);
+            Assert.AreNotEqual("ENTER VALID AMOUNT", summaryOne);
+            Assert.AreNotEqual("PLEASE CHECK YOUR PIN-NUMBER", summaryOne);
+
+            Assert.AreEqual(statment, summaryTwo);
+            Assert.AreNotEqual("PLEASE CHECK YOUR PIN-NUMBER", summaryTwo);
+
+            Assert.AreEqual("PLEASE CHECK YOUR PIN-NUMBER", summaryThree);
+            Assert.AreNotEqual("ENTER VALID AMOUNT", summaryThree);
+            Assert.AreNotEqual(statment, summaryThree);
+
+            Assert.AreEqual("INSUFFICENT BALANCE", summaryFour);
+            Assert.AreNotEqual("ENTER VALID AMOUNT", summaryFour);
+
+            Assert.AreEqual(" PLEASE ENTER THE AMOUNT IN MULTIPLES OF 100", summaryFive);
+            Assert.AreNotEqual("PLEASE CHECK YOUR PIN-NUMBER", summaryFive);
+            Assert.AreNotEqual("INSUFFICENT BALANCE", summaryFive);
+
+        }
+
+        /// <summary>
+        /// To check the mini statement has been added successfully
+        /// </summary>
+        [TestMethod]
+        public void TestSetMiniStatement()
+        {
+            string userName = "Vk";
+            int userPinNumber = 5252;
+            Assert.IsTrue(atm.IsAddCustomerDetail(userName, userPinNumber));
+            DateTime currentDateAndTime = DateTime.Now;
+
+            string summary = atm.SetMiniStatement(" -( WITHDRAW ) ",5000.00);
+            string statment = currentDateAndTime.ToString() + " " + 5000 + " -( WITHDRAW ) ";
+
+            string summaryTwo = atm.SetMiniStatement(" + (DEPOSIT) ", 5000.00);
+            string statmentTwo = currentDateAndTime.ToString() + " " + 5000 + " + (DEPOSIT) ";
+            Assert.AreEqual(statmentTwo, summaryTwo);
+            
+
+        }
+
+        /// <summary>
+        /// To check the mini statement get successfully or not
+        /// </summary>
+        [TestMethod]
+        public void TestGetMiniStatement()
+        {
+            string userName = "Vk";
+            int userPinNumber = 5252;
+            Assert.IsTrue(atm.IsAddCustomerDetail(userName, userPinNumber));
+            DateTime currentDateAndTime = DateTime.Now;
+
+            string summaryDeposit = atm.AddDepositAmount(userName, userPinNumber, 5000.00);
+            string summaryWithdraw = atm.GetWithdrawAmount(userName, userPinNumber, 5000.00);
+            string summaryNotEqual = atm.GetWithdrawAmount("RP", userPinNumber, 5000.00);
+
+            string checkSummary = summaryDeposit + "\n" + summaryWithdraw + "\n";
+            string summary = atm.GetMiniStatement(userName, userPinNumber);
+            Assert.AreEqual(checkSummary, summary);
+            Assert.AreEqual("PLEASE CHECK YOUR PIN-NUMBER", summaryNotEqual);
+
+
+        }
+
+
+    }
+}
